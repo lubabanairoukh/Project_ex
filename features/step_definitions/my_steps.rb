faaacc73520966df4c64ae7e22b29b1fd90b7531
lubaba <lubabana@edu.hac.ac.il>
2023-01-28 19:18:30 +0200

    submit

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -3,20 +3,20 @@ Given(/^I click on Got it button$/) do
   find_element(id:"android:id/button1").click
 end
 
-And(/^i see "([^"]*)" in From header$/) do |value|
+And(/^I see "([^"]*)" in From header$/) do |value|
   actual_value=find_element(id:"header_text_unit_from").text
   if(actual_value !=value)
     fail("Expected value is #{value}, but actual value was #{actual_value}")
   end
 end
 
-And(/^i see "([^"]*)" in To header$/) do |value|
+And(/^I see "([^"]*)" in To header$/) do |value|
   actual_value=find_element(id:"header_text_unit_to").text
   if(actual_value !=value)
     fail("Expected value is #{value}, but actual value was #{actual_value}")
   end
 end
 
-When(/^i click on Swap button$/) do
+When(/^I click on Swap button$/) do
   find_element(id:"fab").click
 end
